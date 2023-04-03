d01dd046a7c3d2376a168558331cfe1ec1353116
Leanhogfutures <61065941+Leanhogfutures@users.noreply.github.com>
2023-04-03 14:58:58 -0700

    Update scripts.js
    v 4.03.23

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,10 +1,22 @@
-<script crossOrigin src="https://unpkg.com/wolfram-notebook-embedder@0.1/dist/wolfram-notebook-embedder.min.js"></script>
+$(document).ready(function() {
+    $('.dropdown').hover(function() {
+      $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    }, function() {
+      $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+    });
+  });
 
-WolframNotebookEmbedder.embed('https://www.wolframcloud.com/obj/darthanchovy0/Published/ForWebsite.nb',document.getElementById('notebookContainer'));
+  
+  // get the navbar element to add text when the user scrolls down
+const navbar = document.getElementById('navbar');
 
-WolframNotebookEmbedder.embed('https://www.wolframcloud.com/obj/tg021/Published/EquiCurv.nb',document.getElementById('notebookContainer'));
-
-WolframNotebookEmbedder.embed('https://www.wolframcloud.com/obj/tg021/Published/Pendulum.nb',document.getElementById('notebookContainer'));
-
-WolframNotebookEmbedder.embed(''https://www.wolframcloud.com/obj/tg021/Published/CubeRender.nb',document.getElementById('notebookContainer'));
-
+// listen for the scroll event on the window
+window.addEventListener('scroll', function() {
+  // if the user has scrolled down past the height of the navbar, add the "scroll" class to the navbar
+  if (window.scrollY > navbar.offsetHeight) {
+    navbar.classList.add('scroll');
+  } else {
+    // otherwise, remove the "scroll" class
+    navbar.classList.remove('scroll');
+  }
+});
