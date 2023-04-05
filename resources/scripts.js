30c6c2a5d84d475c49a051323ef7c42fe496c016
Laplacesol <61065941+Laplacesol@users.noreply.github.com>
2023-04-04 23:18:47 -0700

    Update scripts.js
    nav to top

--- resources/scripts.js
+++ resources/scripts.js
@@ -21,3 +21,25 @@ window.addEventListener('scroll', function() {
   }
 });
 
+$(document).ready(function() {
+    $('.dropdown').hover(function() {
+      $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    }, function() {
+      $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+    });
+  });
+
+  
+  // get the navbar element to add text when the user scrolls down
+const navbar = document.getElementById('navbar');
+
+// listen for the scroll event on the window
+window.addEventListener('scroll', function() {
+  // if the user has scrolled down past the height of the navbar, add the "scroll" class to the navbar
+  if (window.scrollY > navbar.offsetHeight) {
+    navbar.classList.add('scroll');
+  } else {
+    // otherwise, remove the "scroll" class
+    navbar.classList.remove('scroll');
+  }
+}); 
